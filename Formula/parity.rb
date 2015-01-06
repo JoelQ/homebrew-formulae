4a325a4f0d86cfbc927c364f57f6bdda7e78416a
Joël Quenneville <joelq@thoughtbot.com>
2015-01-06 10:57:13 -0500

    Test formula for Parity

--- Formula/parity.rb
+++ Formula/parity.rb
@@ -1,24 +1,16 @@
-require "formula"
-
 class Parity < Formula
-  homepage "https://github.com/croaky/parity"
-  url "https://github.com/croaky/parity/archive/v0.3.1.tar.gz"
-  sha1 "f07b33958067d719a4487243cae10c5f6e1b379c"
+  homepage ""
+  url "https://github.com/JoelQ/parity/releases/download/v0.4.2/parity-0.4.2-osx.tar.gz"
+  sha1 ""
 
   depends_on "heroku-toolbelt"
 
   def install
-    # Add the lib/ directory to the Ruby load path at the top of the file
-    inreplace "lib/parity.rb", /\A/, "$LOAD_PATH << File.dirname(__FILE__)\n\n"
-
     prefix.install "lib" => "lib"
-
-    bin.install "bin/development", "bin/staging", "bin/production"
+    bin.install "bin/development"
   end
 
   test do
     system "#{bin}/development", "--version"
-    system "#{bin}/staging", "--version"
-    system "#{bin}/production", "--version"
   end
 end
